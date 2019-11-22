791e8a9c404a2694ab9fdab31e862bc87c81c545
LincolnRoboticsFTC14298 <46606902+LincolnRoboticsFTC14298@users.noreply.github.com>
2019-11-22 09:33:14 -0800

    Add readme, requirements, and update color model
    Quick readme and requirements text files were added. RGBColorModeling file was fixed so that the numbers were read correctly.

--- RGBColorModeling.py
+++ RGBColorModeling.py
@@ -11,10 +11,9 @@ from matplotlib import colors
 skystone_image = cv2.imread('SkystoneColorRange.png')
 
 # Since plt displays in BGR, we need to convert the RGB image to BGR
-skystone_image = cv2.cvtColor(skystone_image, cv2.COLOR_RGB2BGR)
-
-# plt.imshow(skystone_image)
-# plt.show()
+skystone_image = cv2.cvtColor(skystone_image, cv2.COLOR_BGR2RGB)
+plt.imshow(skystone_image)
+plt.show()
 
 # Turns the 2D skystone_image array into a large array with 3 values each inner array.
 pixel_colors = skystone_image.reshape((np.shape(skystone_image)[0] * np.shape(skystone_image)[1], 3))
@@ -23,14 +22,14 @@ norm.autoscale(pixel_colors)
 pixel_colors = norm(pixel_colors).tolist()
 
 # Split separates the channels
-b, g, r = cv2.split(skystone_image)
+r, g, b = cv2.split(skystone_image)
 fig = plt.figure()
 axis = fig.add_subplot(1, 1, 1, projection="3d")
 
 # Scatter plot
 # Flatten() turns 2D array of values into 1D array
-axis.scatter(b.flatten(), g.flatten(), r.flatten(), facecolors=pixel_colors, marker=".")
-axis.set_xlabel("Blue")
+axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
+axis.set_xlabel("Red")
 axis.set_ylabel("Green")
-axis.set_zlabel("Red")
+axis.set_zlabel("Blue")
 plt.show()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Skystone Experimenting
+Currently just rough code that plays around with the filters and pixels of images of skystones.
+It also graphs the pixel values into a 3D graph.
